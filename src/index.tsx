b0baaf03230a074bd77df11926a6f54d559865f8
sakito <sakito21@gmail.com>
2020-06-24 10:42:41 +0900

    add react tutorial

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+type SquareProps = {
+  value: number;
+};
+
+const Square: React.FC<SquareProps> = ({ value }) => (
+  <button className="square">{value}</button>
+);
+
+const Board: React.FC = () => {
+  const renderSquare = (i: number) => <Square value={i} />;
+  const status = 'Next player: X';
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+const Game: React.FC = () => (
+  <div className="game">
+    <div className="game-board">
+      <Board />
+    </div>
+    <div className="game-info">
+      <div>{/* status */}</div>
+      <ol>{/* TODO */}</ol>
+    </div>
+  </div>
+);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
